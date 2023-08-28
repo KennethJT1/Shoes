501631503d24b87909e349c3763c5b5ac6029ab7
Kenneth <deca@DECAGONs-MacBook-Air.local>
2023-08-28 15:46:15 +0100

    added the hero section

---
+++ src/sections/CustomerReview.jsx
@@ -0,0 +1,6 @@
+
+export const CustomerReview = () => {
+  return (
+    <div>CustomerReview</div>
+  )
+}
---
+++ src/sections/PopularProduct.jsx
@@ -0,0 +1,6 @@
+
+export const PopularProduct = () => {
+  return (
+    <div>PopularProduct</div>
+  )
+}

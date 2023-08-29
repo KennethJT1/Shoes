46ae80dfe4032681c43b483c7e2416be2a424b98
Kenneth <deca@DECAGONs-MacBook-Air.local>
2023-08-29 10:03:02 +0100

    done

--- src/constants/index.js
+++ src/constants/index.js
@@ -137,8 +137,8 @@ export const footerLinks = [
   {
     title: "Get in touch",
     links: [
-      { name: "customer@nike.com", link: "mailto:customer@nike.com" },
-      { name: "+92554862354", link: "tel:+92554862354" },
+      { name: "customer@kjt.com", link: "mailto:customer@kjt.com" },
+      { name: "+2349012121212", link: "tel:+2349012121212" },
     ],
   },
 ];
